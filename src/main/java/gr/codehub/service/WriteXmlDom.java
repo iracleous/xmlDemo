faae30f8b4e16def6ceb7d2e05092caafa8537b0
dimitris <iracleous@gmail.com>
2022-04-08 10:58:22 +0300

    xlst

--- src/main/java/gr/codehub/service/WriteXmlDom.java
+++ src/main/java/gr/codehub/service/WriteXmlDom.java
@@ -1,4 +1,67 @@
 package gr.codehub.service;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class WriteXmlDom {
+    public static void main(String[] args) {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            // root elements
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("company");
+            doc.appendChild(rootElement);
+
+            Element staffElement = doc.createElement("staff");
+            rootElement.appendChild(staffElement);
+
+            Element nameElement = doc.createElement("name");
+            nameElement.setTextContent("Alexis");
+            nameElement.setAttribute("code","45");
+            staffElement.appendChild(nameElement);
+
+            Element element2 = doc.createElement("staff");
+            rootElement.appendChild(element2);
+
+            //...create XML elements, and others...
+
+            // write dom document to a file
+            try (FileOutputStream output =  new FileOutputStream("xml_files/staff-dom.xml")) {
+                writeXml(doc, output);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    // write doc to output stream
+    private static void writeXml(Document doc,  OutputStream output)
+            throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // pretty print
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+        transformer.transform(source, result);
+    }
 }
---
+++ src/main/java/gr/codehub/service/XsltXmlToHtmlDomParser.java
@@ -0,0 +1,44 @@
+package gr.codehub.service;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+
+public class XsltXmlToHtmlDomParser  {
+    private static final String XML_FILENAME    = "xml_files/employees.xml";
+        private static final String XSLT_FILENAME  = "xml_files/staff.xslt";
+
+        public static void main(String[] args) {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            try (InputStream is = new FileInputStream(XML_FILENAME)) {
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(is);
+
+                // transform xml to html via a xslt file
+                try (FileOutputStream output = new FileOutputStream("xml_files/staff.html")) {
+                    transform(doc, output);
+                }
+            } catch (IOException | ParserConfigurationException |
+                    SAXException | TransformerException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static void transform(Document doc, OutputStream output)  throws TransformerException {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            // add XSLT in Transformer
+            Transformer transformer = transformerFactory.newTransformer(
+                    new StreamSource(new File(XSLT_FILENAME)));
+            transformer.transform(new DOMSource(doc), new StreamResult(output));
+        }
+}
